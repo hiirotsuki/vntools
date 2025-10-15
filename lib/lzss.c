2955cd2922f60970894f59176bd80e105f9e5771
Ørjan Malde <red@foxi.me>
2025-10-15 16:25:42 +0200

    lib/lzss.[ch]: in-memory lzss decompression
    Signed-off-by: Ørjan Malde <red@foxi.me>

--- lib/lzss.c
+++ lib/lzss.c
@@ -10,9 +10,71 @@ unsigned char *lzss_window_init(int window_size, const char fill_byte)
 	return window;
 }
 
+void lzss_window_reset(unsigned char *window, int window_size, const char fill_byte)
+{
+	memset(window, fill_byte, window_size);
+}
+
 void lzss_window_free(void *ptr)
 {
 	if(ptr)
 		free(ptr);
 	ptr = NULL;
+}
+
+int lzss_decompress(unsigned char *output, unsigned long output_size, const unsigned char *input, unsigned long input_size)
+{
+	unsigned char *window;
+	unsigned int lzss_pos = 4078;
+	unsigned long input_pos = 0;
+	unsigned long output_pos = 0;
+	int cbit, control, match_pos, match_len;
+
+	window = lzss_window_init(4096, 0x20);
+	if(!window)
+		return -1;
+
+	while(input_pos < input_size && output_pos < output_size)
+	{
+		control = input[input_pos++];
+
+		for(cbit = 0x01; cbit & 0xff; cbit <<= 1)
+		{
+			if(output_pos >= output_size)
+				break;
+
+			if(input_pos >= input_size)
+				break;
+
+			if(control & cbit)
+			{
+				/* Literal byte */
+				output[output_pos++] = window[lzss_pos++] = input[input_pos++];
+				lzss_pos &= 4095;
+			}
+			else
+			{
+				if(input_pos + 1 >= input_size)
+					break;
+
+				match_pos = input[input_pos++];
+				match_len = input[input_pos++];
+				match_pos |= (match_len & 0xf0) << 4;
+				match_len = (match_len & 0x0f) + 3;
+
+				while(match_len--)
+				{
+					if(output_pos >= output_size)
+						break;
+
+					output[output_pos++] = window[lzss_pos++] = window[match_pos++];
+					lzss_pos &= 4095;
+					match_pos &= 4095;
+				}
+			}
+		}
+	}
+
+	lzss_window_free(window);
+	return 0;
 }
--- lib/lzss.h
+++ lib/lzss.h
@@ -3,5 +3,7 @@
 
 unsigned char *lzss_window_init(int window_size, const char fill_byte);
 void lzss_window_free(void *ptr);
+int lzss_decompress(unsigned char *output, unsigned long output_size, 
+                    const unsigned char *input, unsigned long input_size);
 
 #endif /* LZSS_H */
